202e533a8a96866c07e4bf8d9e871fde2ddb12a0
Emika Tashiro Hammond <eth@bu.edu>
2025-03-23 15:14:26 -0400

    add function to test execution via GitHub Actions

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "add.h"
+#include <iostream>
+using std::cout;
+
+int main () {
+
+	int a = 10;
+	int b = 12;
+
+	int sum = add(10,12);
+	cout << a << " + " << b << " = " << sum << "\n";
+}
