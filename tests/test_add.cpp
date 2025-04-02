bd35a2c35db0205a1982529c07e5c48cad5b45a6
dywang19 <dywang19@bu.edu>
2025-04-02 17:15:42 -0400

    Update test_add.cpp

--- tests/test_add.cpp
+++ tests/test_add.cpp
@@ -3,7 +3,7 @@
 
 // Test for add() - Should pass after changes
 TEST(AddTest, AddFunction) {
-    EXPECT_EQ(add(2, 3), 5);
-    EXPECT_EQ(add(-1, 1), 0);
+    EXPECT_EQ(add(2, 3), -1);
+    EXPECT_EQ(add(-1, 1), -2);
 }
 
